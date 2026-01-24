4cf19b84a6668be21e667728997e46f3b7e50f5e
Kai1708 <imakid546@gmail.com>
2026-01-23 22:24:43 -0500

    file position update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
-        popup: resolve(__dirname, 'index.html'),
-        background: resolve(__dirname, 'src/assets/background.ts'),
-        content: resolve(__dirname, 'src/assets/content.ts'),
+        popup: "index.html",
+        background: "src/background.ts",
+        content: "src/content.ts"
       },
       output: {
-        entryFileNames: '[name].js',
-      },
-    },
-  },
+        entryFileNames: "[name].js"
+      }
+    }
+  }
 })
